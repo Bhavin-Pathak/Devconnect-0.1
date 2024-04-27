6200ba62674073488c648536a4a19de402ea8525
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-27 18:44:09 +0530

    chore: Mediaquery added

---
+++ lib/utils/screen_size.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenW;
+  static double? screenH;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenW = _mediaQueryData!.size.width;
+    screenH = _mediaQueryData!.size.height;
+  }
+}
