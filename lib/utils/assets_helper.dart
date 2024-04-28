5cbbda179fabead7bd80052b0b5e1e8cc7f55d49
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-28 12:03:06 +0530

    chore: extt change

--- lib/utils/assets_helper.dart
+++ lib/utils/assets_helper.dart
@@ -1,7 +1,7 @@
 // ignore: camel_case_types
 class assetsImageHelper {
   static const String splash = "assets/splash/splash.png";
-  static const String onbording1 = "assets/images/onbording1.jpg";
-  static const String onbording2 = "assets/images/onbording2.jpg";
-  static const String onbording3 = "assets/images/onbording3.jpg";
+  static const String onbording1 = "assets/images/onbording1.png";
+  static const String onbording2 = "assets/images/onbording2.png";
+  static const String onbording3 = "assets/images/onbording3.png";
 }
